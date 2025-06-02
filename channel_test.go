d44e220aef03b9d1aaeabf1f30a8c6931de97193
Yohan Apriyandi <cepoangkuningan@gmail.com>
2025-06-02 13:08:36 +0700

    add select channel and range channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajar_golang_goroutine
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -27,7 +28,7 @@ func GiveMeResponse(channel chan string) {
 	channel <- "Yohan Apriyandi"
 }
 
-func nlTestChannelAsParameter(t *testing.T) {
+func TestChannelAsParameter(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
 
@@ -78,4 +79,46 @@ func TestBufferedChannel(t *testing.T) {
 	}()
 	time.Sleep(5 * time.Second)
 	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 100; i++ {
+			channel <- "Perualangan ke " + strconv.Itoa(i)
+		}
+		defer close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Range ", data)
+	}
+	fmt.Println("Selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Ambil data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Ambil data dari channel 2", data)
+			counter++
+		}
+
+		if counter == 2 {
+			break
+		}
+	}
 }
